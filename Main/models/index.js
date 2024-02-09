8f579c36a406ab2051ed005d4123f7c670ba6350
e6m9 <alostsound@gmail.com>
2024-02-09 17:07:15 -0500

    changed models

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,13 +1,30 @@
 const User = require('./User');
 const Pet = require('./Pet');
+const Favorite = require('./Favorite');
 
-User.hasMany(Pet, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+// User.hasMany(Pet, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Pet.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+User.belongsToMany(Pet, {
+  through: {
+    model: Favorite,
+    unique: false
+  },
+  as: 'favorite_pets'
 });
 
-Pet.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+Pet.belongsToMany(User, {
+  through: {
+    Favorite,
+    unique: false
+  },
+  as: 'starred_pets'
+})
 
-module.exports = { User, Pet };
+module.exports = { User, Pet, Favorite };
